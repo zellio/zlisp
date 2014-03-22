f371787526dfdd6786f59c8537aa0bc1a046779e
Zachary Elliott <zach@nyu.edu>
2014-03-22 12:29:45 -0400

    Adding zeme/types header

---
+++ include/types.h
@@ -0,0 +1,16 @@
+#ifndef __ZEME_TYPES_H__
+#define __ZEME_TYPES_H__ 1
+
+#include "type/object.h"
+#include "type/boolean.h"
+#include "type/character.h"
+#include "type/fixnum.h"
+#include "type/symbol.h"
+#include "type/pair.h"
+#include "type/string.h"
+#include "type/vector.h"
+#include "type/port.h"
+#include "type/builtin.h"
+#include "type/closure.h"
+
+#endif
