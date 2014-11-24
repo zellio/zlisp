44896eb1c64387da11d0c23800ed485aa81ab3e0
Zachary Elliott <zach@nyu.edu>
2014-11-23 23:16:52 -0500

    Base core

---
+++ include/lang/core.h
@@ -0,0 +1,22 @@
+#ifndef __ZL_LANG_CORE_H__
+#define __ZL_LANG_CORE_H__ 1
+
+#include "lang/type.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#if 0
+}
+#endif
+
+
+object_t *zlc_eq(object_t *obj1, object_t *obj2);
+
+object_t *zlc_assoc(object_t *key, object_t *alist);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif
---
+++ lib/lang/core.c
@@ -0,0 +1,21 @@
+#include "lang/core.h"
+
+object_t *zlc_eq(object_t *obj1, object_t *obj2)
+{
+    if (is_pair(obj1) || is_pair(obj2))
+        return ZLC_NIL;
+
+    return (obj1 == obj2) ? ZLC_TRUE : ZLC_FALSE;
+}
+
+object_t *zlc_assoc(object_t *key, object_t *alist)
+{
+    if (!is_pair(alist))
+        error("Type error in `zlc_assoc'");
+
+    for (object_t **node = &alist; is_pair(*node); node = &pair_cdr(*node))
+        if (is_pair(*node) && object_compare(pair_car(*node), key) == 0)
+            return *node;
+
+    return ZLC_NIL;
+}
